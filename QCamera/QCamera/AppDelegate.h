962e069759c01220cb38f978d2eede8de41a5fb4
huya_qiancheng <practiceqian@foxmail.com>
2020-05-19 00:15:21 +0800

    项目基本配置

---
+++ QCamera/QCamera/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  QCamera
+//
+//  Created by huya_qiancheng on 2020/5/18.
+//  Copyright © 2020 cccheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ QCamera/QCamera/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  QCamera
+//
+//  Created by huya_qiancheng on 2020/5/18.
+//  Copyright © 2020 cccheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
